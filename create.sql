f5c4ac7740e48c75384bce31561fe8dc93e2b15b
Rina372 <114817013+Rina372@users.noreply.github.com>
2023-02-20 21:01:36 +0300

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS genre (
+	genre_id SERIAL PRIMARY KEY,
+	genre_name VARCHAR(60) NOT null
+);
+
+CREATE TABLE IF NOT EXISTS singers (
+	singer_ID SERIAL PRIMARY KEY,
+	singer_name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS list_genres (
+	ID SERIAL PRIMARY KEY,
+	genre_ID INTEGER NOT NULL REFERENCES Genre(Genre_ID),
+	singer_ID INTEGER NOT NULL REFERENCES Singers(Singer_ID)
+);
+
+CREATE TABLE IF NOT EXISTS albums (
+	album_ID SERIAL PRIMARY KEY,
+	album_name VARCHAR(60) NOT NULL,
+	release_date INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS list_albums (
+	ID SERIAL PRIMARY KEY,
+	singer_ID INTEGER NOT NULL REFERENCES Singers(Singer_ID),
+	album_ID INTEGER NOT NULL REFERENCES Albums(Album_ID)
+);
+
+CREATE TABLE IF NOT EXISTS tracks (
+	track_ID SERIAL PRIMARY KEY,
+	track_name VARCHAR(60) NOT NULL,
+	album_ID INTEGER NOT NULL REFERENCES Albums(Album_ID),
+	duration NUMERIC	
+);
+
+CREATE TABLE IF NOT EXISTS collection (
+	collection_id SERIAL PRIMARY KEY,
+	collection_name VARCHAR(60) NOT NULL,
+	release_date INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS collection_tracks (
+	ID SERIAL PRIMARY KEY,
+	track_ID INTEGER NOT NULL REFERENCES Tracks(Track_ID),
+	collection_id INTEGER NOT NULL REFERENCES Collection(Collection_id)
+);
+
